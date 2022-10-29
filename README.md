36957bb6150f4170bfe8a4445562ac784cc57652
Jakub Stibůrek <alcolp96@gmail.com>
2022-10-29 14:48:09 +0200

    dump file contents to stdout option

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ Cat man page: [cat](https://man7.org/linux/man-pages/man1/cat.1.html)
 
 ## Todo
 - [x] Copy standard input to standard output
-- [ ] Dump contents of a given file to standard output
+- [x] Dump contents of a given file to standard output
+- [ ] Number lines option
+- [ ] Number nonempty lines
+- [ ] Show $ at end of each line option
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
+use std::error::Error;
 use std::io;
+use clap::{Parser};
+use std::fs::{read_to_string};
+
+#[derive(Parser, Debug)]
+#[command(author = "Jakub Stiburek", version = "0.1.0", about = "Concatenate to standard output.")]
+struct Args {
+    file: Option<String>,
+}
 
 fn main() {
-    default();
+    let args = Args::parse();
+    
+    match args.file {
+        Some(file) => dump_file(file).unwrap(),
+        None => default()
+    }
+}
+
+fn dump_file(input_file: String) -> Result<(), Box<dyn Error>> {
+    println!("{}", read_to_string(input_file).unwrap());
+    
+    Ok(())
 }
 
 fn default() {
