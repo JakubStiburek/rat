ffbd9be0eb7715ecd3c6caa4c6bb7e6dc5e08b14
Jakub Stibůrek <alcolp96@gmail.com>
2022-10-29 14:01:56 +0200

    stdi copy to stdo

---
+++ README.md
@@ -0,0 +1,10 @@
+# rat
+
+Rat is a *cat* util clone written in rust - rust cat = rat.
+
+Cat man page: [cat](https://man7.org/linux/man-pages/man1/cat.1.html)
+
+
+## Todo
+- [x] Copy standard input to standard output
+- [ ] Dump contents of a given file to standard output
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    default();
+}
+
+fn default() {
+    loop {
+        let mut input = String::new();
+        
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        
+        println!("{input}");
+    }
 }
