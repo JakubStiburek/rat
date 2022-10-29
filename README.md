4456ef5535cf474bba8028ed5bc55ad846aff318
Jakub Stibůrek <alcolp96@gmail.com>
2022-10-29 17:52:21 +0200

    add prefix and suffix options

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ Cat man page: [cat](https://man7.org/linux/man-pages/man1/cat.1.html)
 ## Todo
 - [x] Copy standard input to standard output
 - [x] Dump contents of a given file to standard output
-- [ ] Number lines option
+- [x] Number lines option
 - [ ] Number nonempty lines
-- [ ] Show $ at end of each line option
+- [x] Show $ at end of each line option
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::fmt::format;
 use std::io;
 use clap::{Parser};
 use std::fs::{read_to_string};
@@ -6,20 +7,62 @@ use std::fs::{read_to_string};
 #[derive(Parser, Debug)]
 #[command(author = "Jakub Stiburek", version = "0.1.0", about = "Concatenate to standard output.")]
 struct Args {
+    #[arg(short, long)]
+    number: bool,
+    
+    #[arg(short, long)]
+    end: bool,
+    
     file: Option<String>,
+}
+
+#[derive(Eq, PartialEq)]
+enum Flag {
+    NUMBER,
+    END,
 }
 
 fn main() {
     let args = Args::parse();
     
+    let mut flags: Vec<Flag> = vec![];
+    
+    if args.number {
+        flags.push(Flag::NUMBER)
+    }
+    
+    if args.end {
+        flags.push(Flag::END)
+    }
+    
     match args.file {
-        Some(file) => dump_file(file).unwrap(),
+        Some(file) => dump_file(file, flags).unwrap(),
         None => default()
     }
 }
 
-fn dump_file(input_file: String) -> Result<(), Box<dyn Error>> {
-    println!("{}", read_to_string(input_file).unwrap());
+fn dump_file(input_file: String, flags: Vec<Flag>) -> Result<(), Box<dyn Error>> {
+    if flags.is_empty() {
+        println!("{}", read_to_string(input_file).unwrap());
+    } else {
+        let content = read_to_string(input_file).unwrap();
+        let lines = content.split("\n");
+        for (i, line) in lines.enumerate() {
+            let mut prefix: String = "".to_string();
+            let mut suffix: String = "".to_string();
+            
+            if flags.contains(&Flag::NUMBER) {
+                prefix = format!("{}", i).to_string();
+            }
+            if flags.contains(&Flag::END) {
+                suffix = "$".to_string();
+            }
+            println!("{}:{}:{}", prefix, line, suffix );
+        }
+    }
+    
+    
+    
     
     Ok(())
 }
